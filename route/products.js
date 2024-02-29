f48e39ac78e9195a13bd73c947e3a12073bb7460
Dmytro Shkatula <idnol84@gmail.com>
2024-02-29 23:07:31 +0200

    updated

--- route/products.js
+++ route/products.js
@@ -8,15 +8,4 @@ const router = express.Router();
 router.get("/", getAll);
 router.get("/recommended", getRecommended);
 
-// router.get("/:id", getById);
-// router.post("/", authenticate, validateUserRights, validateBody(schemas.addSchema), add);
-// router.put(
-//     "/:id",
-//     authenticate,
-//     validateUserRights,
-//     validateBody(schemas.updateSchema),
-//     updateById
-// );
-// router.delete("/:id", authenticate, validateUserRights, deleteById);
-
 module.exports = router;
