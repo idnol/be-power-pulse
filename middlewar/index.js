fb4858d23ade5b483da05593295b7971cd68f91d
Dmytro Shkatula <idnol84@gmail.com>
2024-03-03 15:53:53 +0200

    added auth and deleted not used functions

--- middlewar/index.js
+++ middlewar/index.js
@@ -1,11 +1,9 @@
-const validateUserRights = require("./validateUserRights");
 const validateBody = require("./validateBody");
 const handleMongooseError = require("./handleMongooseError");
 const wrapper = require("./wrapper");
 const authenticate = require("./authenticate");
 
 module.exports = {
-    validateUserRights: validateUserRights,
     validateBody: validateBody,
     handleMongooseError: handleMongooseError,
     wrapper: wrapper,
